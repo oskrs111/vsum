d4b6a5ab973a0f3d555805edf2a03f02ab4a3911
Oscar Sanz <oskrs111@gmail.com>
2014-02-21 16:32:34 +0100

    Working on demo

---
+++ VSUM/demo/page2.php
@@ -0,0 +1,40 @@
+<?php include('vsum-page.php'); ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>VSUM - Demo</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href="css/global.css" rel="stylesheet" type="text/css" media="screen" charset="utf-8" />
+</head>
+<body>
+<div id="wrapper">
+  <div id="logo"> <a href="#"><img src="img/logo.png" alt="" /></a> </div>
+  <div id="nav">
+    <ul>
+      <li><a href="page2.php">Page 1</a></li>
+      <li>/</li>
+      <li><a href="#"><b>Page 2</b></a></li>
+      <li>/</li>
+      <li><a href="page3.php">Page 3</a></li>
+      <li>/</li>
+      <li><a href="page4.php">Page 4</a></li>
+	  <li>/</li>
+      <li><a href="/php/vsum/vsum-logout.php">LOGOUT</a></li>	 	  
+    </ul>
+  </div>
+  <div id="main">    
+    <div class="left">
+	<br/><br/><br/><br/>
+	<h1>Now you are logged on Page 2</h1>
+    </div>    
+	<div  class="right">
+  </div>
+</div>
+<div id="footer">
+  <ul>
+    <li>&copy;2014 VSUM demo page based on </li>    
+    <li>'Studio980' CSS Template designed by <a href="http://www.skyrocketlabs.com/">Skyrocket Labs</a></li>
+  </ul>
+</div>
+</body>
+</html>
